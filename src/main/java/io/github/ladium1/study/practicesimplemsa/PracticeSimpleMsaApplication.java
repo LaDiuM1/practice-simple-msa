1dcb122c438236e98ebf4f6b6f756ec11a4287ca
LaDiuM <wjddhrtjd123@gmail.com>
2026-06-09 11:48:41 +0900

    Initial commit

---
+++ src/main/java/io/github/ladium1/study/practicesimplemsa/PracticeSimpleMsaApplication.java
@@ -0,0 +1,13 @@
+package io.github.ladium1.study.practicesimplemsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSimpleMsaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSimpleMsaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/ladium1/study/practicesimplemsa/PracticeSimpleMsaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ladium1.study.practicesimplemsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSimpleMsaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
